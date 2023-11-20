275a8eed1663ce821e9563a624cff43d49e96f73
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-11-20 23:19:54 +0530

    JJJJJJ

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post('/upload', upload.single('htmlFile'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
